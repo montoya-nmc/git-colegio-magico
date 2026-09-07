69bd0648e604ad14ccdff9266e0fbf521d1d315f
montoya-nmc <olcenico578@gmail.com>
2026-09-07 18:21:31 +0000

    feat: Reto 7 resuelto

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,7 +6,16 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+
+         int mayor = tamanos[0];
+
+        for (int i = 1; i < tamanos.length; i++) {
+            if (tamanos[i] > mayor) {
+                mayor = tamanos[i];
+            }
+        }
+        return mayor; // TODO: Cambia esto
+
     }
 
     public static void main(String[] args) {
